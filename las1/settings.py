af55e35f71fda606725e2f70022dde34cb481321
Tia Fields <tia.fields226@gmail.comy>
2024-01-14 15:41:13 -0500

    static switch for debug mode

--- las1/settings.py
+++ las1/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'accounts/static/')
+    ]
+else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'accounts/static/')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'accounts/static/'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
